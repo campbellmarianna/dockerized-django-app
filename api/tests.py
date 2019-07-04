892cfbbe2b29d816fd15193f4be8529644d8dec9
Marianna <mckd2018@gmail.com>
2019-07-03 20:38:22 -0700

    [add] :white_check_mark: 2 tests in  in each app

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,16 @@
+from django.http import HttpRequest
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from workout.models import Workout
+
+from . import views
+
+class APITests(TestCase):
+    def test_api_status_code(self):
+            response = self.client.get('/api/')
+            self.assertEqual(response.status_code, 200)
+
+    def test_workouts_list_status_code(self):
+            response = self.client.get('/api/workout/')
+            self.assertEqual(response.status_code, 200)
--- workout/tests.py
+++ workout/tests.py
@@ -1,43 +1,18 @@
-import datetime
-
+from django.http import HttpRequest
 from django.test import TestCase
-from django.utils import timezone, reverse
+from django.urls import reverse
+
 
 from .models import Workout
 
-# Model check whether idea works
-# View checks website functionality
-    # 200 if it is there end to end
+from . import views
 
-# create workout with a date
-class WorkoutModelTests(TestCase):
-    def test_was_posted_recently_with_future_workout(self):
-        """
-        was_posted_recently() returns False for workouts whose post_date is
-        in the future.
-        """
-        time = timezone.now() + datetime.timedelta(days=30)
-        future_workout = Workout(post_date=time)
-        self.assertIs(future_question.was_posted_recently(), False)
+class WebsiteTests(TestCase):
 
-    def create_workout(day):
-        """
-        Create a workout with the given `day`.
-        """
-        time = timezone.now() + datetime.timedelta(day=day)
-        return Workout.objects.create(day=day, post_date=time)
+    def test_index_page_status_code(self):
+        response = self.client.get('/workouts')
+        self.assertEquals(response.status_code, 301) # Permanent Redirect
 
-# once the workout is generated there should be three sets
-    def test_has_three_sets(self):
-        """
-        Workout returns true if it has three workout sets otherwise return false.
-        """
-        # 1. Create a workout with three sets and save
-        # 2. assertEqual workout.sets_set.count() == 3
-        workout1 = Workout.objects.get(pk=1)
-        workout2 = Workout.objects.get(pk=2)
-        workout3 = Workout.objects.get(pk=3)
-        self.assertEqual(workout.)
-
-
-# test the workout index view, if there are no workouts post a message
+    def test_admin_page_status_code(self):
+        response = self.client.get('/admin/')
+        self.assertEquals(response.status_code, 302) # Found
